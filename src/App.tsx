27d74d6ad1bc85425fb660411ec2061eb1d6d247
huckjoo <omp0109@naver.com>
2023-12-04 23:12:25 +0900

    style: header css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import './App.css';
 
+import { FaYoutube } from 'react-icons/fa';
+import { IoIosSearch } from 'react-icons/io';
+
 function App() {
-  console.log('hello');
   return (
     <>
-      <p className="text-3xl font-bold underline">YOUTUBE CLONE</p>
-      <button>클릭</button>
+      {/** Header */}
+      <div className="dark:bg-slate-900 flex p-3 justify-between items-center">
+        <div className="flex">
+          <button className="text-2xl mr-1.5 text-red-500">
+            <FaYoutube />
+          </button>
+          <span className="text-2xl dark:text-white">Youtube</span>
+        </div>
+        <div className="w-96">
+          <form className="flex">
+            <input
+              type="text"
+              className="text-xl mr-2 text-white w-full rounded-md border-0 bg-slate-900 placeholder:text-gray-400 ring-1 ring-inset ring-gray-600"
+              placeholder="Search"
+            ></input>
+            <button className="text-2xl dark:text-white bg-slate-700 p-2">
+              <IoIosSearch />
+            </button>
+          </form>
+        </div>
+        <div className=""></div>
+      </div>
     </>
   );
 }

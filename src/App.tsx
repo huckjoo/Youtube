05d5e804d620c35201a4a0693f723b93c8ef8b8d
huckjoo <omp0109@naver.com>
2023-12-08 15:44:01 +0900

    feat: 목업 생성 후 목업 기반 contents 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
 import './App.css';
 
-import { FaYoutube } from 'react-icons/fa';
-import { IoIosSearch } from 'react-icons/io';
+import Header from './components/Header';
+import { mostPopularAPI } from './mocks/mostPopularAPI';
 
 function App() {
   return (
     <>
-      {/** Header */}
-      <div className="dark:bg-slate-900 flex p-3 justify-between items-center">
-        <div className="flex-1 shrink-0">
-          <button className="text-2xl mr-1.5 text-red-500">
-            <FaYoutube />
-          </button>
-          <span className="text-2xl dark:text-white">Youtube</span>
-        </div>
-        <div className="w-96 flex-[2_1_0]">
-          <form className="flex">
-            <input
-              type="text"
-              className="text-xl mr-2 text-white w-full rounded-md border-0 bg-slate-900 placeholder:text-gray-400 ring-1 ring-inset ring-gray-600"
-              placeholder="Search"
-            ></input>
-            <button className="text-2xl dark:text-white bg-slate-700 p-2">
-              <IoIosSearch />
-            </button>
-          </form>
-        </div>
-        <div className="flex-1"></div>
+      <Header />
+      {/** Contents */}
+      <div className="dark:bg-slate-950 h-full w-full bg-cover flex flex-wrap justify-center">
+        {mostPopularAPI.items.map((item) => (
+          <div key={item.id} className="dark:text-white p-2 max-w-xs">
+            <img src={item.snippet.thumbnails.medium.url} />
+            <div id="details" className="pt-2">
+              <h3 className="overflow-hidden text-ellipsis font-semibold line-clamp-2">
+                {item.snippet.title}
+              </h3>
+              <span className=" dark:text-gray-600">
+                {item.snippet.channelTitle}
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import { FaYoutube } from 'react-icons/fa';
+import { IoIosSearch } from 'react-icons/io';
+
+const Header = () => {
+  return (
+    <div className="dark:bg-slate-950 flex p-3 justify-between items-center">
+      <div className="flex-1 shrink-0">
+        <button className="text-2xl mr-1.5 text-red-500">
+          <FaYoutube />
+        </button>
+        <span className="text-2xl dark:text-white">Youtube</span>
+      </div>
+      <div className="w-96 flex-[2_1_0]">
+        <form className="flex">
+          <input
+            type="text"
+            className="text-xl mr-2 text-white w-full rounded-md border-0 bg-slate-950 placeholder:text-gray-400 ring-1 ring-inset ring-gray-600"
+            placeholder="Search"
+          ></input>
+          <button className="text-2xl dark:text-white bg-slate-800 p-2 rounded-md">
+            <IoIosSearch />
+          </button>
+        </form>
+      </div>
+      <div className="flex-1"></div>
+    </div>
+  );
+};
+
+export default Header;

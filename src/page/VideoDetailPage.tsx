be2a7b8b0f577b008fc0064e38c09ae6e5131539
huckjoo <omp0109@naver.com>
2023-12-30 22:40:12 +0900

    fix: channel id 넘겨서 channel list 불러오기

--- src/page/VideoDetailPage.tsx
+++ src/page/VideoDetailPage.tsx
@@ -1,6 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { useLocation, useParams } from 'react-router-dom';
 
+interface Item {
+  snippet: {
+    localized: {
+      title: string;
+      description: string;
+    };
+    thumbnails: {
+      medium: {
+        url: string;
+      };
+    };
+  };
+}
+
+interface ChannelData {
+  items: Item[];
+}
+
 const VideoDetailPage = () => {
   const { videoId } = useParams();
   const { state: channelId } = useLocation();
@@ -14,9 +32,9 @@ const VideoDetailPage = () => {
   //   return response;
   // };
 
-  const fetchChannelsData = async () => {
+  const fetchChannelsData = async (): Promise<ChannelData> => {
     const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${
+      `https://youtube.googleapis.com/youtube/v3/playlists?part=snippet%2CcontentDetails&channelId=${channelId}&maxResults=20&key=${
         import.meta.env.VITE_YOUTUBE_API_KEY
       }`,
     ).then((res) => res.json());
@@ -32,15 +50,18 @@ const VideoDetailPage = () => {
 
   if (error) return 'An error has occureed: ' + error.message;
 
-  console.log('data', data);
-
   return (
     <div className="dark:bg-slate-950 h-full w-full bg-cover flex flex-wrap justify-center">
       <div id="video" className="flex-[2_1_0] shrink-0 dark:bg-slate-200">
         비디오 들어갈 자리
       </div>
       <div className="flex-1 shrink-0 dark:bg-slate-500">
-        비디오 리스트 들어갈 자리
+        {data?.items.map((item) => (
+          <div>
+            <img src={item.snippet.thumbnails.medium.url} />
+            <span>{item.snippet.localized.title}</span>
+          </div>
+        ))}
       </div>
     </div>
   );

81d2cd5cf1edb0b1dbb6dd1bde37beed0db46b1b
huckjoo <omp0109@naver.com>
2024-01-14 11:55:07 +0900

    feat: 헤더 home으로 돌아가는 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,14 +15,22 @@ const Header = () => {
     navigate(`/videos/${inputText}`);
   };
 
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setInputText('');
+    navigate(`/`);
+  };
+
   return (
     <div className="relative mb-16">
       <div className="dark:bg-slate-950 flex p-3 justify-between items-center fixed top-0 left-0 right-0">
-        <div className="flex-1 shrink-0">
+        <div className="flex-1 shrink-0 " onClick={handleClick}>
           <button className="text-2xl mr-1.5 text-red-500">
             <FaYoutube />
           </button>
-          <span className="text-2xl dark:text-white">Youtube</span>
+          <span className="text-2xl dark:text-white cursor-pointer">
+            Youtube
+          </span>
         </div>
         <div className="w-96 flex-[2_1_0]">
           <form className="flex" onSubmit={handleSubmit}>
